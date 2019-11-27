1d60b359935d248a2e6a1e0c280fb2d5d1a2a79d
Sarah <eaaskh@students.eaaa.dk>
2019-11-27 10:42:44 +0100

    Basic models setup

---
+++ kogebogen/Models/Ingredients.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kogebogen.Models
+{
+    public class Ingredients
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public string? Unit { get; set; }
+
+        public Ingredients (string name, int amount, string? unit)
+        {
+            this.Name = name;
+            this.Amount = amount;
+            this.Unit = unit;
+        }
+    }
+}
---
+++ kogebogen/Repository/Repository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using kogebogen.Models;
+
+namespace kogebogen.Repository
+{
+    public class Repository
+    {
+        public List<Recipe> CookBook;
+
+        public Ingredients Salt;
+        public Ingredients Pepper;
+        public Ingredients Onion;
+        public Ingredients Garlic;
+        public Ingredients Beef;
+        public Ingredients TomatoSauce;
+        public Ingredients Oregano;
+
+        public Repository()
+        {
+            Recipe Lasagna = new Recipe();
+            Lasagna.Ingredients.Add(Pepper);
+            Lasagna.Ingredients.Add(Salt);
+            Lasagna.Ingredients.Add(Beef);
+            Lasagna.Ingredients.Add(Oregano);
+            Lasagna.Ingredients.Add(Onion);
+            Lasagna.Ingredients.Add(Garlic);
+            Lasagna.Ingredients.Add(TomatoSauce);
+            CookBook.Add(Lasagna);
+
+        }
+    }
+}
