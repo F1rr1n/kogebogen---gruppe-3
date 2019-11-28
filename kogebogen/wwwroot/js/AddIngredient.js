8fadb56d981bf033985342bd58d99d00792fe4a3
NearlyBatman <denskyldige@hotmail.com>
2019-11-28 11:37:27 +0100

    Add recipe part 1

--- kogebogen/wwwroot/js/AddIngredient.js
+++ kogebogen/wwwroot/js/AddIngredient.js
@@ -1,8 +1,24 @@
-﻿
-
-function meh()
+﻿function micro()
 {
-    let k = document.getElementById("ingredient");
-    var cln = k.cloneNode(true);
-    document.getElementById("ingredients").appendChild(cln);
+
+    let k = document.createElement("p");
+    var output = '<input type="text" required/>' + '<input type="number" value="amount" required/>' +
+        '<select required>' +
+        '<option value="stk">stk</option>' +
+        '<option value="g">g</option>' +
+        '<option value="dl">dl</option>' +
+        '<option value="tsk">tsk</option>' +
+        '<option value="spsk">spsk</option>' +
+        '</select>';
+    k.innerHTML += output;
+    document.getElementById("ingredient").appendChild(k);
+}
+
+function test()
+{
+
+    var k = '<li><input type="text" required/></li>';
+    var test = document.getElementById("description_steps");
+    test.innerHTML += k;
+
 }
