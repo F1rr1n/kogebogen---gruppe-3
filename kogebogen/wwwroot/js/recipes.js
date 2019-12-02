c486398ebfc2220adfb682c2af41cee4f21079b6
Sarah <eaaskh@students.eaaa.dk>
2019-12-02 10:02:56 +0100

    Recipes
    With add button and on input instead of on change

--- kogebogen/wwwroot/js/recipes.js
+++ kogebogen/wwwroot/js/recipes.js
@@ -1,7 +1,7 @@
 ﻿var coll = document.getElementsByClassName("collapsible");
 
 for (var i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
+    coll[i].addEventListener("click", function () {
         this.classList.toggle("active");
         var content = this.nextElementSibling;
         if (content.style.display === "block") {
@@ -17,10 +17,10 @@ var filterPrice = document.getElementById("filterPrice");
 var filterTime = document.getElementById("filterTime");
 
 for (var i = 0; i < ranges.length; i++) {
-    filterPrice.innerHTML = "Max pris: " + ranges[0].value + " kr";
-    filterTime.innerHTML = "Tid i alt: " + ranges[1].value + " min";
-    ranges[i].addEventListener("change", function () {
+    filterPrice.innerHTML = "Max pris: " + ranges[i].value + " kr";
+    filterTime.innerHTML = "Tid i alt: " + ranges[i].value + " min";
+    ranges[i].addEventListener("input", function () {
         filterPrice.innerHTML = "Max pris: " + ranges[0].value + " kr";
         filterTime.innerHTML = "Tid i alt: " + ranges[1].value + " min";
-    });
+    }, false);
 }
