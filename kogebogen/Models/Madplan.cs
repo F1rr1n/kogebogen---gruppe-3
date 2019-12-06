573206cbc1bddc0fe7f8a527b2d3de89c193a9bc
Sarah <eaaskh@students.eaaa.dk>
2019-12-06 09:50:31 +0100

    Madplan forsøg

---
+++ kogebogen/Models/Madplan.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace kogebogen.Models
+{
+    public class Madplan
+    {
+        Days[] days = { new Days("Mandag"), new Days("Tirsdag"), new Days("Onsdag"), new Days("Torsdag"), new Days("Fredag"), new Days("Lørdag"), new Days("Søndag") };
+    }
+}
