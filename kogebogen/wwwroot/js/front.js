288d681b7ef19f0c051e16d23ee56d67bae5e54a
Sarah <eaaskh@students.eaaa.dk>
2019-12-12 08:55:40 +0100

    Styling recipes, favorites, myRecipes

--- kogebogen/wwwroot/js/front.js
+++ kogebogen/wwwroot/js/front.js
@@ -1,14 +1,4 @@
-﻿document.getElementById("searchModal").style.display = "none";
-document.getElementById("searchButton").addEventListener('click', function () {
-    var search = document.getElementById("searchModal");
-    if (search.style.display == "none") {
-        search.style.display = "block";
-    } else {
-        search.style.display = "none";
-    }
-});
-
-var profile = document.getElementById("theProfile");
+﻿var profile = document.getElementById("theProfile");
 
 //Gode gammeldags click funktioner, der enten displayer none eller block på profil div
 document.getElementById("openProfile").addEventListener('click', function () {
@@ -16,5 +6,15 @@ document.getElementById("openProfile").addEventListener('click', function () {
         profile.style.left = "0vw";
         profile.style.animationName = "in";
         profile.style.boxShadow = "2px 0px 10px 0px";
+    }
+});
+
+document.getElementById("searchModal").style.display = "none";
+document.getElementById("searchButton").addEventListener('click', function () {
+    var search = document.getElementById("searchModal");
+    if (search.style.display == "none") {
+        search.style.display = "block";
+    } else {
+        search.style.display = "none";
     }
 });
--- kogebogen/wwwroot/js/site.js
+++ kogebogen/wwwroot/js/site.js
@@ -8,8 +8,18 @@ document.getElementById("back").addEventListener('click', function () {
     }
 });
 
+var search = document.getElementById("searchModal");
+search.style.display = "none";
+document.getElementById("searchButtonNav").addEventListener('click', function () {
+    if (search.style.display == "none") {
+        search.style.display = "block";
+    } else {
+        search.style.display = "none";
+    }
+});
+
 //Swipe functions - så der er mulighed for at trække profilen ind
-document.addEventListener('swiped-right', function () {
+document.getElementById("swipeField").addEventListener('swiped-right', function () {
     profile.style.left = "0vw";
     profile.style.animationName = "in";
     profile.style.boxShadow = "2px 0px 10px 0px";
@@ -19,4 +29,4 @@ document.addEventListener('swiped-left', function () {
     profile.style.left = "-70vw";
     profile.style.animationName = "out";
     profile.style.boxShadow = "none";
-})
+}) 
