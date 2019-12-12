fcdfd2b5c759b26e2cd9950c1c418e6174864fd0
NearlyBatman <denskyldige@hotmail.com>
2019-12-12 11:05:48 +0100

    AddToFavList

--- kogebogen/Controllers/HomeController.cs
+++ kogebogen/Controllers/HomeController.cs
@@ -100,7 +100,11 @@ namespace kogebogen.Controllers
             return View("Index");
 
         }
-        public IActionResult Addfav(int id)
+        public IActionResult Addfav()
+        {
+            return View(user);
+        }
+        public void AddFavToList(int id)
         {
             // Finder opskriften baseret på dets ID og sætter det på et nyt Recipe objekt
             Recipe recFav = repo.CookBook.Find(rec => rec.ID == id);
@@ -113,9 +117,7 @@ namespace kogebogen.Controllers
             {
                 user.Favorites.Remove(recFav);
             }
-            return View(user);
         }
-
         [HttpPost]
         public IActionResult AddToFoodPlan(int id)
         {
